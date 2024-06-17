e50306d5b715a1c5fe3f06f6315595e187d5366a
Mostafa Bakr <159573166+MostafaBkr@users.noreply.github.com>
2024-06-17 04:32:38 +0300

    Update SMS.cpp

--- SMS.cpp
+++ SMS.cpp
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+g++ <source-file> -o <executable>
 
 #include <array>
 #include <chrono> // For delay
