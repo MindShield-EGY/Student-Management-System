2dc8d56e1aae93b212ce2b254e2a927d5c42c041
Mostafa Bakr <159573166+MostafaBkr@users.noreply.github.com>
2024-06-17 04:42:41 +0300

    Update SMS.cpp

--- SMS.cpp
+++ SMS.cpp
@@ -22,6 +22,19 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+cmake_minimum_required(VERSION 3.10)
+
+# Project name and version
+project(MyProject VERSION 1.0)
+
+# Specify the C++ standard
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+# Add the executable
+add_executable(SMS.cpp)
+
 g++ <source-file> -o <executable>
 
 #include <array>
